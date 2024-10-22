5f15e909cf4835329877a048125deb2f7da761d2
linuxdev <ivnaas@gmail.com>
2024-10-22 20:22:47 +0200

    update app.py

--- app.py
+++ app.py
@@ -22,15 +22,15 @@ anObj = callAngelAPI()
 # Function to log stock data every 5 minutes
 def log_stock_data():
     while True:
-        time.sleep(30)  # Sleep for 5 minutes
+        time.sleep(30)  # Sleep for 30 seconds
         for stock in portfolio_stocks:
             try:
                 data = get_stock_data(stock)
                 closeprice = data.indicators["close"]
                 rsi15min = data.indicators["RSI"]
 
-                st = callAngelInd(anObj,stock)
-                strend_value = round(st['Supertrend_Value'],2)
+                st = callAngelInd(anObj, stock)
+                strend_value = round(st['Supertrend_Value'], 2)
                 strend_direction = st['Supertrend_Direction']
 
                 # Create log entry with IST timestamp
@@ -48,30 +48,38 @@ logging_thread = threading.Thread(target=log_stock_data, daemon=True)
 logging_thread.start()
 
 app.layout = html.Div(children=[
-    html.Div(children=[
-        html.H1(children='Stock Tracker'),
-        dcc.Input(id='stock-symbol', value='SIEMENS', type='text'),
-        html.Button('Submit', id='submit-button', n_clicks=0),
-        html.Div(id='output-container'),
-        html.Div(id='day-high-low'),
-        dcc.Interval(id='interval-component', interval=30*1000, n_intervals=0)  # Interval set to 30 seconds
-    ], style={'width': '60%', 'display': 'inline-block'}),
-
-    html.Div(children=[
-        html.H2('Portfolio'),
-        dcc.Textarea(id='portfolio-input', value='SIEMENS,TCS', style={'width': '100%', 'height': 100}),
-        html.Button('Update Portfolio', id='portfolio-button', n_clicks=0),
+    dcc.Tabs([
+        dcc.Tab(label='Portfolio', children=[
+            html.Div(children=[
+                html.H1(children='Stock Tracker'),
+                dcc.Input(id='stock-symbol', value='SIEMENS', type='text'),
+                html.Button('Submit', id='submit-button', n_clicks=0),
+                html.Div(id='output-container'),
+                html.Div(id='day-high-low'),
+                dcc.Interval(id='interval-component', interval=30*1000, n_intervals=0)  # Interval set to 30 seconds
+            ], style={'width': '60%', 'display': 'inline-block'}),
+            
+            html.Div(children=[
+                html.H2('Portfolio'),
+                dcc.Textarea(id='portfolio-input', value='SIEMENS,TCS', style={'width': '100%', 'height': 100}),
+                html.Button('Update Portfolio', id='portfolio-button', n_clicks=0),
+            ], style={'width': '35%', 'display': 'inline-block', 'float': 'right'}),
+            
+            html.Div(children=[
+                html.H2('Logs'),
+                html.Ul(id='log-container'),
+            ], style={'width': '60%', 'display': 'inline-block', 'float': 'left'})
+        ]),
         
-        html.H2('Watchlist'),
-        dcc.Textarea(id='watchlist-input', value='ABB,BAJAJFIN', style={'width': '100%', 'height': 100}),
-        html.Button('Update Watchlist', id='watchlist-button', n_clicks=0),
-
-    ], style={'width': '35%', 'display': 'inline-block', 'float': 'right'}),
-
-    html.Div(children=[
-        html.H2('Logs'),
-        html.Ul(id='log-container'),
-    ], style={'width': '60%', 'display': 'inline-block', 'float': 'left'})
+        dcc.Tab(label='Watchlist', children=[
+            html.Div(children=[
+                html.H2('Watchlist'),
+                dcc.Textarea(id='watchlist-input', value='ABB,BAJAJFIN', style={'width': '100%', 'height': 100}),
+                html.Button('Update Watchlist', id='watchlist-button', n_clicks=0),
+                html.Div(id='watchlist-output'),
+            ], style={'width': '60%', 'display': 'inline-block'}),
+        ])
+    ])
 ])
 
 @app.callback(
@@ -94,9 +102,10 @@ def update_output(n_clicks, n_intervals, portfolio_clicks, watchlist_clicks, val
     
     if watchlist_clicks > 0:
         watchlist_stocks = [stock.strip() for stock in watchlist_input.split(',') if stock.strip()]
-
+    
     output = []
     log_output = [html.Li(entry) for entry in reversed(log_entries)]
+    
     # Update based on the button click for stock data
     if n_clicks > 0:
         try:
@@ -104,8 +113,7 @@ def update_output(n_clicks, n_intervals, portfolio_clicks, watchlist_clicks, val
             rsi15min = data.indicators["RSI"]
             closeprice = data.indicators["close"]
 
-
-            st = callAngelInd(anObj,value)
+            st = callAngelInd(anObj, value)
             strend_value = st['Supertrend_Value']
             strend_direction = st['Supertrend_Direction']
 
@@ -113,7 +121,7 @@ def update_output(n_clicks, n_intervals, portfolio_clicks, watchlist_clicks, val
                 html.Li(f"Stock Symbol: {value}"),
                 html.Li(f"RSI_15min: {str(round(rsi15min, 2))}"),
                 html.Li(f"Last Close Price: {str(closeprice)}"),
-                html.Li(f"SuperTrend Value: {str(round(strend_value,2))}"),
+                html.Li(f"SuperTrend Value: {str(round(strend_value, 2))}"),
                 html.Li(f"SuperTrend Direction: {strend_direction}")
             ]
         except Exception as e:
@@ -123,5 +131,3 @@ def update_output(n_clicks, n_intervals, portfolio_clicks, watchlist_clicks, val
 
 if __name__ == '__main__':
     app.run_server(debug=True)
-
-
